3a4438b105bff65a15478cbe7a2e109267dc2650
Anelize Nardelli <nardellianelize20@gmail.com>
2026-02-27 12:29:50 -0300

    Soluciona o exercíicio 2, proposto em aula, utilizando a implementação manual da estrutura de dados 'Array'.

---
+++ exemplos/arrays/exercicio-2.js
@@ -0,0 +1,21 @@
+// Exercício 2
+
+// Enunciado: O setor de Recursos Humanos de uma empresa deseja armazenar os nomes dos funcionários que participaram de um treinamento.
+
+import Array from "../../estruturas-de-dados/Array.js";
+
+const funcionariosTreinados = new Array();
+
+funcionariosTreinados.adicionarElementoNoFinal("Anelize Nardelli");
+funcionariosTreinados.adicionarElementoNoFinal("Eduardo Petarnella");
+funcionariosTreinados.adicionarElementoNoFinal("João Pedro Surita");
+funcionariosTreinados.adicionarElementoNoFinal("Bruno Oller");
+
+funcionariosTreinados.obterElementoPorIndice(2);
+
+funcionariosTreinados.limparArray();
+
+// Pergunta 1: O que acontece se tentarmos acessar um índice inexistente após limpar o Array?
+funcionariosTreinados.obterElementoPorIndice(2);
+// Recebemos o retorno definido caso o índice solicitado não estivesse ao alcance.
+// Nesse caso, temos que "O índice não é válido para o Array".
